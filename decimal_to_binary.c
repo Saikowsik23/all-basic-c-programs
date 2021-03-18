394dea0fea9bbf1195336ad51903f7c11277dcb8
Saikowsik23 <74443882+Saikowsik23@users.noreply.github.com>
2021-03-18 09:42:45 +0530

    Create decimal_to_binary.c

---
+++ decimal_to_binary.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+//enter the decimal value
+    scanf("%d",&N);
+     int b[32];
+     int length;
+  //if  the enter number is 0 then the binary is 0
+     if(N==0){
+         printf("0");
+     }else{
+       //if the number divided by 2 then that intitial number is 0 else 1 and it goes on.
+       //this prints in reverse order.
+    for(int i=0;N>0;i++){
+        
+        if(N%2==0){
+            b[i]=0;
+        }else {
+            b[i]=1;
+        }
+            N=N/2;
+            length=i;
+    }
+     //this prints it in right order
+    printf("1");
+    for(int i=length-1;i>=0;i--){
+       printf("%d",b[i]);
+    }
+     }
+}
