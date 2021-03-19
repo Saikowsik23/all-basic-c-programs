4685bbe646a0e589d63f8e53d3b986bf340ada85
Saikowsik23 <74443882+Saikowsik23@users.noreply.github.com>
2021-03-19 15:02:28 +0530

    Update insert_new_element.c

--- insert_new_element.c
+++ insert_new_element.c
@@ -2,22 +2,18 @@
 int main()
 {
    int n;
-  printf("enter the number of elements:\n");
     //enter number of elements
     scanf("%d",&n);
     int arr[n];
     //enter the elements of an array
-  printf("enter the elements in array:\n");
     for(int i=0;i<n;i++){
       scanf("%d",&arr[i]);  
      } 
     arr[n]=0;
     //enter new element
-  printf("enter the new element:\n");
     int ne;
     scanf("%d",&ne);
     //enter position
-  printf("enter the position:\n");
     int pos;
     scanf("%d",&pos);
     //making 0 @ position k
@@ -28,7 +24,6 @@ int main()
     }//inserting element @ position k
     arr[pos]=ne;
     //printing
-  printf("the array is:\n");
     for(int i=0;i<=n;i++){
      printf("%d ",arr[i]);  
     }
